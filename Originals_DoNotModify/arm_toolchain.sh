027d10cb0fa752a67b67f97b9537743612db74f0
Godwin Saure <Raphaeltesorero@hotmail.com>
2024-03-17 23:19:24 -0600

    Added annotations to both files + gcc/64-bit default for x86
    added comments to both toolchain files and made 64-bit default + added GCC for the x86 toolchain specifically

--- Originals_DoNotModify/arm_toolchain.sh
+++ Originals_DoNotModify/arm_toolchain.sh
@@ -5,7 +5,9 @@
 # August 2022
 
 
+# if no arguments are given when executing...
 if [ $# -lt 1 ]; then
+	# print instruction on how to use the file
 	echo "Usage:"
 	echo ""
 	echo "arm_toolchain.sh  [-p | --port <port number, default 12222>] <assembly filename> [-o | --output <output filename>]"
@@ -17,19 +19,26 @@ if [ $# -lt 1 ]; then
 	echo "-q | --qemu                   Run executable in QEMU emulator. This will execute the program."
 	echo "-p | --port                   Specify a port for communication between QEMU and GDB. Default is 12222."
 	echo "-o | --output <filename>      Output filename."
-	
+	# then exit program (with error)
 	exit 1
 fi
 
-POSITIONAL_ARGS=()
-GDB=False
-OUTPUT_FILE=""
-VERBOSE=False
-QEMU=False
-PORT="12222"
-BREAK="main"
-RUN=False
+# various variables we will use later
+# will be used for the args above (if prompted by user)
+POSITIONAL_ARGS = ()
+GDB = False
+OUTPUT_FILE = ""
+VERBOSE = False
+QEMU = False
+PORT = "12222"
+BREAK = "main"
+RUN = False
+
+# loop through all arguments
 while [[ $# -gt 0 ]]; do
+	# for each arg, raise the flag (= TRUE) to run the command later
+	# also set any relevant variables as necessary (e.g. user-given PORT)
+	# also note the special case where arg doesn't match anything
 	case $1 in
 		-g|--gdb)
 			GDB=True
@@ -75,15 +84,19 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
+
+# check if the given file exists, if not exit program with error
 if [[ ! -f $1 ]]; then
 	echo "Specified file does not exist"
 	exit 1
 fi
 
+# default output file name = input file (if user does not give the name)
 if [ "$OUTPUT_FILE" == "" ]; then
 	OUTPUT_FILE=${1%.*}
 fi
 
+# actually do the verbose command if prompted; print relevant arguments
 if [ "$VERBOSE" == "True" ]; then
 	echo "Arguments being set:"
 	echo "	GDB = ${GDB}"
@@ -100,17 +113,16 @@ if [ "$VERBOSE" == "True" ]; then
 
 fi
 
-# Raspberry Pi 3B
+# Raspberry Pi 3B (the actual compiling)
 arm-linux-gnueabihf-gcc -ggdb -mfpu=vfp -march=armv6+fp -mabi=aapcs-linux $1 -o $OUTPUT_FILE -static -nostdlib &&
 
-
+# notify user (might as well)
 if [ "$VERBOSE" == "True" ]; then
-
 	echo "Compiling finished"
-	
 fi
 
 
+# if QEMO command is prompted with GDB = false, run QEMO then exit program
 if [ "$QEMU" == "True" ] && [ "$GDB" == "False" ]; then
 	# Only run QEMU
 	echo "Starting QEMU ..."
@@ -119,34 +131,40 @@ if [ "$QEMU" == "True" ] && [ "$GDB" == "False" ]; then
 	qemu-arm $OUTPUT_FILE && echo ""
 
 	exit 0
-	
+
+# if prompted with GDB = true... (read comment below)	
 elif [ "$QEMU" == "False" ] && [ "$GDB" == "True" ]; then
 	# Run QEMU in remote and GDB with remote target
 
 	echo "Starting QEMU in Remote Mode listening on port $PORT ..."
 	qemu-arm -g $PORT $OUTPUT_FILE &
 	
-	
+	# use local port for QEMO in remote mode
 	gdb_params=()
 	gdb_params+=(-ex "target remote 127.0.0.1:${PORT}")
 	gdb_params+=(-ex "b ${BREAK}")
 
+	# note to add run command if given by user
 	if [ "$RUN" == "True" ]; then
 
 		gdb_params+=(-ex "r")
 
 	fi
-
+	
+	# do the actual GDB in remote mode
 	echo "Starting GDB in Remote Mode connecting to QEMU ..."
 	gdb-multiarch "${gdb_params[@]}" $OUTPUT_FILE &&
 
 	exit 0
 
+# other case, self-explanatory
 elif [ "$QEMU" == "False" ] && [ "$GDB" == "False" ]; then
 	# Don't run either and exit normally
 
 	exit 0
 
+# if QEMO and GDB = true, notify user "error" and run file
+# in QEMO instead
 else
 	echo ""
 	echo "****"
--- Originals_DoNotModify/x86_toolchain.sh
+++ Originals_DoNotModify/x86_toolchain.sh
@@ -4,8 +4,9 @@
 # ISS Program, SADT, SAIT
 # August 2022
 
-
+# if no arguments are given when executing...
 if [ $# -lt 1 ]; then
+	# print instruction on how to use the file
 	echo "Usage:"
 	echo ""
 	echo "x86_toolchain.sh [ options ] <assembly filename> [-o | --output <output filename>]"
@@ -17,19 +18,26 @@ if [ $# -lt 1 ]; then
 	echo "-q | --qemu                   Run executable in QEMU emulator. This will execute the program."
 	echo "-64| --x86-64                 Compile for 64bit (x86-64) system."
 	echo "-o | --output <filename>      Output filename."
-
+	# then exit program (with error)
 	exit 1
 fi
 
-POSITIONAL_ARGS=()
-GDB=False
-OUTPUT_FILE=""
-VERBOSE=False
-BITS=False
-QEMU=False
-BREAK="_start"
-RUN=False
+# various variables we will use later
+# will be used for the args above (if prompted by user)
+POSITIONAL_ARGS = ()
+GDB = False
+OUTPUT_FILE = ""
+VERBOSE = False
+BITS = True # set 64-bit as the default (as per readme)
+QEMU = False
+BREAK = "_start"
+RUN = False
+
+# loop through all arguments
 while [[ $# -gt 0 ]]; do
+	# for each arg, raise the flag (= TRUE) to run the command later
+	# also set any relevant variables as necessary (e.g. user-given break point)
+	# also note the special case where arg doesn't match anything
 	case $1 in
 		-g|--gdb)
 			GDB=True
@@ -74,15 +82,18 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
+# check if the given file exists, if not exit program with error
 if [[ ! -f $1 ]]; then
 	echo "Specified file does not exist"
 	exit 1
 fi
 
+# default output file name = input file (if user does not give the name)
 if [ "$OUTPUT_FILE" == "" ]; then
 	OUTPUT_FILE=${1%.*}
 fi
 
+# actually do the verbose command if prompted; print relevant arguments
 if [ "$VERBOSE" == "True" ]; then
 	echo "Arguments being set:"
 	echo "	GDB = ${GDB}"
@@ -99,48 +110,38 @@ if [ "$VERBOSE" == "True" ]; then
 
 fi
 
+# compile with NASM as 64 bit or otherwise depending on user input
 if [ "$BITS" == "True" ]; then
-
 	nasm -f elf64 $1 -o $OUTPUT_FILE.o && echo ""
 
 
 elif [ "$BITS" == "False" ]; then
-
 	nasm -f elf $1 -o $OUTPUT_FILE.o && echo ""
 
 fi
 
+# notify user about NASM compilation (might as well)
 if [ "$VERBOSE" == "True" ]; then
-
-	echo "NASM finished"
-	echo "Linking ..."
-	
-fi
-
-if [ "$VERBOSE" == "True" ]; then
-
 	echo "NASM finished"
 	echo "Linking ..."
 fi
 
+# same with NASM but for ld (NOTE: replaced with gcc as per readme)
 if [ "$BITS" == "True" ]; then
-
-	ld -m elf_x86_64 $OUTPUT_FILE.o -o $OUTPUT_FILE && echo ""
+	gcc -m64 -o $OUTPUT_FILE $1 && echo ""
 
 
 elif [ "$BITS" == "False" ]; then
-
-	ld -m elf_i386 $OUTPUT_FILE.o -o $OUTPUT_FILE && echo ""
+	gcc -m32 -o $OUTPUT_FILE $1 && echo ""
 
 fi
 
-
+# notify user about ld as well 
 if [ "$VERBOSE" == "True" ]; then
-
 	echo "Linking finished"
-
 fi
 
+# run program in QEMU in 64 bit or otherwise based on user input (if prompted)
 if [ "$QEMU" == "True" ]; then
 
 	echo "Starting QEMU ..."
@@ -160,6 +161,7 @@ if [ "$QEMU" == "True" ]; then
 	
 fi
 
+# same as QEMU but for gdb. However, note the parameters given by the user as well (as well as if the user wants to run the file)
 if [ "$GDB" == "True" ]; then
 
 	gdb_params=()
